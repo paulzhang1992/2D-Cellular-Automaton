45459f762f89d187f7277a658f8c0416a8f714a3
paul <paulzhang19921113@gmail.com>
2019-03-15 09:46:23 -0400

    Naming changes

--- edu.neu.csye6200.ma/src/Test.java
+++ edu.neu.csye6200.ma/src/Test.java
@@ -2,11 +2,11 @@ public class Test {
 
 
     public static void main(String[] args) {
-        GridCell gc = GridCell.getInstance();
+        GridSquareCell gc = GridSquareCell.getInstance();
         //RuleAdjacent ra = new RuleAdjacent();
         int [] entryPoint = {0,0};
         //ra.applyRule(gc.getCell(entryPoint));
-        SquareCell sc = gc.getCell(entryPoint);
+        CellSquare sc = gc.getCell(entryPoint);
         System.out.println(sc.left().up().gridCoord);
     }
 }
